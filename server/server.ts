4d58f10cf3546e0464a434c04b2ce51f35f25435
Nick <nickgillham@proton.me>
2023-10-13 10:03:39 +0100

    build

--- server/server.ts
+++ server/server.ts
@@ -1,15 +1,22 @@
 import dotenv from 'dotenv'
 dotenv.config()
 import express, { Router } from 'express'
+import path from 'path'
 var cors = require('cors')
 
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000
 
 app.use(cors(), express.json())
 app.use('/api/fact', require('./Routes/fact'))
 app.use('/api/kaibaChat', require('./Routes/kaibaChat'))
+// serve static assets in production
+app.use(express.static('client/dist'))
 
-app.listen(port, '0.0.0.0', () => {
-	console.log(`Server running on http://localhost:${port}`)
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+})
+
+app.listen(PORT, () => {
+	console.log(`Server running on http://localhost:${PORT}`)
 })
